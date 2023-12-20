e48865a9fc30a4e970940b76be4d6a2c2fd324f6
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2023-12-21 02:01:17 +0800

    one_hot_encode.py
    One-hot encoding of train, valid and test labels of the AIDER image class

---
+++ one_hot_encode.py
@@ -0,0 +1,43 @@
+# One hot encode all label training array.
+
+from numpy import array
+from numpy import argmax
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+
+# integer encode
+label_encoder = LabelEncoder()
+integer_encoded = label_encoder.fit_transform(new_y)
+
+print()
+
+# binary encode
+onehot_encoder = OneHotEncoder(sparse=False)
+integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+
+# One hot encode all valid array.
+
+# integer encode
+label_encoder = LabelEncoder()
+integer_encoded = label_encoder.fit_transform(new_y_valid)
+
+print()
+
+# binary encode
+onehot_encoder = OneHotEncoder(sparse=False)
+integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+onehot_encodedvalid = onehot_encoder.fit_transform(integer_encoded)
+
+# One hot encode all test array.
+
+# integer encode
+label_encoder = LabelEncoder()
+integer_encoded = label_encoder.fit_transform(new_y_test)
+
+print()
+
+# binary encode
+onehot_encoder = OneHotEncoder(sparse=False)
+integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+onehot_encodedtest = onehot_encoder.fit_transform(integer_encoded)
