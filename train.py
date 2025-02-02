ea8495ca6f8a4af2e6d68fd10ba10eb14fd5b9ea
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2025-02-02 17:40:41 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,14 +1,34 @@
-# initiate RMSprop optimizer
+############ Data Augmentation ##############
+
+train_datagen =tf.keras.preprocessing.image.ImageDataGenerator(horizontal_flip=True,vertical_flip=False)
+valid_datagen =tf.keras.preprocessing.image.ImageDataGenerator(horizontal_flip=False, vertical_flip=False)
+
+train = train_datagen.flow(new_X,onehot_encoded,batch_size=16)
+valid = valid_datagen.flow(new_X_valid,onehot_encodedvalid,batch_size=16)
+
+######### initiate RMSprop optimizer #############
 
 from keras import optimizers
-opt = optimizers.RMSprop(lr=0.0001, decay=1e-6)  # 0.0001, 1e-6
+opt = optimizers.RMSprop(learning_rate=0.0001, decay=1e-6)  
 
-# Cosine Similarity
-import tensorflow as tf
+############# Combined Loss ####################
 
-cosine_sim = tf.keras.losses.CosineSimilarity(axis=1)
+class Total_Loss(tf.keras.losses.Loss):
+    def __init__(self):
+        super().__init__()
+    def call(self, y_true, y_pred):
+      # Custom CE Loss
+      log_y_pred = tf.keras.ops.log(y_pred)
+      elements = -tf.math.multiply_no_nan(x=log_y_pred, y=y_true)
+      CE_loss = tf.reduce_mean(tf.reduce_sum(elements,axis=1))
+      # Cosine sim Loss
+      cos_sim = -tf.reduce_sum(tf.nn.l2_normalize(y_true) * tf.nn.l2_normalize(y_pred), axis = -1)
+      cossim = tf.reduce_mean(cos_sim)
+      return 0.1*cossim + 0.9*CE_loss
 
-########### Use code from MBonvblocks.py #####################################
-efficient_net_b0.compile(loss= [cosine_sim, 'categorical_crossentropy'], loss_weights=[1.,1.] , optimizer=opt, metrics=[tf.keras.metrics.CosineSimilarity()])
+################# Compile ########################
 
-history = efficient_net_b0.fit(train, validation_data= valid, epochs=300, batch_size = 10, verbose=1)
+Watteffnet36.compile(loss = [Total_Loss], optimizer = opt, metrics = ['accuracy'])
+
+################# Run Training ###########################
+history = Watteffnet36.fit(train, validation_data= valid, epochs= 600, batch_size = 64, verbose=1)
